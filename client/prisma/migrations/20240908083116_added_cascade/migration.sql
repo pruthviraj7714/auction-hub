c6daf148b9755d36b62f5e1c307b77297cc093be
pruthviarj7714 <marganepruthviraj@gmail.com>
2024-09-08 15:14:49 +0530

    added cascade to db

---
+++ client/prisma/migrations/20240908083116_added_cascade/migration.sql
@@ -0,0 +1,17 @@
+-- DropForeignKey
+ALTER TABLE "Auction" DROP CONSTRAINT "Auction_productId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Bid" DROP CONSTRAINT "Bid_auctionId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Bid" DROP CONSTRAINT "Bid_userId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Bid" ADD CONSTRAINT "Bid_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Bid" ADD CONSTRAINT "Bid_auctionId_fkey" FOREIGN KEY ("auctionId") REFERENCES "Auction"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Auction" ADD CONSTRAINT "Auction_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
